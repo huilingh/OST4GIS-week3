582db69f2fc3665177ba365bb849bbeb8cd3cb5d
huilingh <35381107+huilingh@users.noreply.github.com>
2018-02-01 20:01:32 -0500

    Huiling He - lab

--- lab/lab1/part2.js
+++ lab/lab1/part2.js
@@ -7,7 +7,7 @@ Functions can be passed as values to other functions. Each exercise here builds 
 Instructions: Write a function that *always* returns the number 1.
 ===================== */
 
-var justOne = function() {};
+var justOne = function() {return 1;};
 
 console.log('justOne success:', justOne() === 1);
 
@@ -15,7 +15,7 @@ console.log('justOne success:', justOne() === 1);
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-var isEven = function() {};
+var isEven = function(num) {return num % 2 === 0;};
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -24,7 +24,7 @@ Instructions: Write a function that *always* returns false.
               Use functions "justOne" and "isEven" somehow in the definition.
 ===================== */
 
-var justFalse = function() {};
+var justFalse = function() {return false};
 
 console.log('justFalse success:', justFalse() === false);
 
@@ -32,7 +32,7 @@ console.log('justFalse success:', justFalse() === false);
 Instructions: Write a function that takes a boolean value and returns its opposite.
 ===================== */
 
-var not = function() {};
+var not = function(statement) {return true};
 
 console.log('not success:', not(true) === false);
 
@@ -41,7 +41,7 @@ Instructions: Write a function that returns true if a number is odd
               Use functions "isEven" and "not" somehow in the definition.
 ===================== */
 
-var isOdd = function() {};
+var isOdd = function(num) {return num % 2 !== 0};
 
 console.log('isOdd success:', isOdd(4) === false);
 
@@ -49,7 +49,7 @@ console.log('isOdd success:', isOdd(4) === false);
 Instructions: Write a function that takes a list of numbers and returns a list with only numbers above 10
 ===================== */
 
-var filterOutLessThan10 = function() {};
+var filterOutLessThan10 = _.each(array, function (num) {return num >=10;})
 
 console.log('filterOutLessThan10 success:', filterOutLessThan10([4, 11]) === [11]);
 
@@ -63,4 +63,3 @@ Instructions: Let's bring it all together. Write a function that filters a list 
 var filter = function(array, func) {};
 
 console.log('filter success:', filter([4, 11], isOdd) === [11]);
-
--- lab/lab2/part3-underscore-refactor.js
+++ lab/lab2/part3-underscore-refactor.js
@@ -32,79 +32,83 @@
 
 
   // clean data
-  for (var i = 0; i < schools.length - 1; i++) {
-    // If we have '19104 - 1234', splitting and taking the first (0th) element
-    // as an integer should yield a zip in the format above
-    if (typeof schools[i].ZIPCODE === 'string') {
-      split = schools[i].ZIPCODE.split(' ');
+  // My new codes
+_.each(schools, function(obj) {
+    if (_.isString(obj.ZIPCODE)){
+      var split;
+      var normalized_zip;
+      split = obj.ZIPCODE.split(' ');
       normalized_zip = parseInt(split[0]);
-      schools[i].ZIPCODE = normalized_zip;
+      obj.ZIPCODE = normalized_zip;
     }
 
-    // Check out the use of typeof here — this was not a contrived example.
-    // Someone actually messed up the data entry
-    if (typeof schools[i].GRADE_ORG === 'number') {  // if number
-      schools[i].HAS_KINDERGARTEN = schools[i].GRADE_LEVEL < 1;
-      schools[i].HAS_ELEMENTARY = 1 < schools[i].GRADE_LEVEL < 6;
-      schools[i].HAS_MIDDLE_SCHOOL = 5 < schools[i].GRADE_LEVEL < 9;
-      schools[i].HAS_HIGH_SCHOOL = 8 < schools[i].GRADE_LEVEL < 13;
-    } else {  // otherwise (in case of string)
-      schools[i].HAS_KINDERGARTEN = schools[i].GRADE_LEVEL.toUpperCase().indexOf('K') >= 0;
-      schools[i].HAS_ELEMENTARY = schools[i].GRADE_LEVEL.toUpperCase().indexOf('ELEM') >= 0;
-      schools[i].HAS_MIDDLE_SCHOOL = schools[i].GRADE_LEVEL.toUpperCase().indexOf('MID') >= 0;
-      schools[i].HAS_HIGH_SCHOOL = schools[i].GRADE_LEVEL.toUpperCase().indexOf('HIGH') >= 0;
+    if (_.isNumber(obj.GRADE_ORG)) {
+      obj.HAS_KINDERGARTEN = obj.GRADE_LEVEL < 1;
+      obj.HAS_ELEMENTARY = 1 < obj.GRADE_LEVEL < 6;
+      obj.HAS_MIDDLE_SCHOOL = 5 < obj.GRADE_LEVEL < 9;
+      obj.HAS_HIGH_SCHOOL = 8 < obj.GRADE_LEVEL < 13;
+    } else {
+      obj.HAS_KINDERGARTEN = obj.GRADE_LEVEL.toUpperCase().indexOf('K') >= 0;
+      obj.HAS_ELEMENTARY = obj.GRADE_LEVEL.toUpperCase().indexOf('ELEM') >= 0;
+      obj.HAS_MIDDLE_SCHOOL = obj.GRADE_LEVEL.toUpperCase().indexOf('MID') >= 0;
+      obj.HAS_HIGH_SCHOOL = obj.GRADE_LEVEL.toUpperCase().indexOf('HIGH') >= 0;
     }
-  }
+    return obj;
+  })
+
 
   // filter data
   var filtered_data = [];
   var filtered_out = [];
-  for (var i = 0; i < schools.length - 1; i++) {
-    isOpen = schools[i].ACTIVE.toUpperCase() == 'OPEN';
-    isPublic = (schools[i].TYPE.toUpperCase() !== 'CHARTER' ||
-                schools[i].TYPE.toUpperCase() !== 'PRIVATE');
-    isSchool = (schools[i].HAS_KINDERGARTEN ||
-                schools[i].HAS_ELEMENTARY ||
-                schools[i].HAS_MIDDLE_SCHOOL ||
-                schools[i].HAS_HIGH_SCHOOL);
-    meetsMinimumEnrollment = schools[i].ENROLLMENT > minEnrollment;
-    meetsZipCondition = acceptedZipcodes.indexOf(schools[i].ZIPCODE) >= 0;
-    filter_condition = (isOpen &&
-                        isSchool &&
-                        meetsMinimumEnrollment &&
-                        !meetsZipCondition);
 
-    if (filter_condition) {
-      filtered_data.push(schools[i]);
-    } else {
-      filtered_out.push(schools[i]);
-    }
+// My new codes
+_.each(schools, function(obj) {
+  isOpen = obj.ACTIVE.toUpperCase() == 'OPEN';
+  isPublic = (obj.TYPE.toUpperCase() !== 'CHARTER' ||
+              obj.TYPE.toUpperCase() !== 'PRIVATE');
+  isSchool = (obj.HAS_KINDERGARTEN ||
+              obj.HAS_ELEMENTARY ||
+              obj.HAS_MIDDLE_SCHOOL ||
+              obj.HAS_HIGH_SCHOOL);
+  meetsMinimumEnrollment = obj.ENROLLMENT > minEnrollment;
+  meetsZipCondition = acceptedZipcodes.indexOf(obj.ZIPCODE) >= 0;
+  filter_condition = (isOpen &&
+                      isSchool &&
+                      meetsMinimumEnrollment &&
+                      !meetsZipCondition);
+
+  if (filter_condition) {
+    filtered_data.push(obj);
+  } else {
+    filtered_out.push(obj);
   }
-  console.log('Included:', filtered_data.length);
-  console.log('Excluded:', filtered_out.length);
+})
+
+console.log('Included:', filtered_data.length);
+console.log('Excluded:', filtered_out.length);
+
 
   // main loop
   var color;
-  for (var i = 0; i < filtered_data.length - 1; i++) {
-    isOpen = filtered_data[i].ACTIVE.toUpperCase() == 'OPEN';
-    isPublic = (filtered_data[i].TYPE.toUpperCase() !== 'CHARTER' ||
-                filtered_data[i].TYPE.toUpperCase() !== 'PRIVATE');
-    meetsMinimumEnrollment = filtered_data[i].ENROLLMENT > minEnrollment;
 
-    // Constructing the styling  options for our map
-    if (filtered_data[i].HAS_HIGH_SCHOOL){
-      color = '#0000FF';
-    } else if (filtered_data[i].HAS_MIDDLE_SCHOOL) {
-      color = '#00FF00';
-    } else {
-      color = '##FF0000';
-    }
-    // The style options
-    var pathOpts = {'radius': filtered_data[i].ENROLLMENT / 30,
-                    'fillColor': color};
-    L.circleMarker([filtered_data[i].Y, filtered_data[i].X], pathOpts)
-      .bindPopup(filtered_data[i].FACILNAME_LABEL)
-      .addTo(map);
+  // My new codes
+_.each(filtered_data, function(obj) {
+  isOpen = obj.ACTIVE.toUpperCase() == 'OPEN';
+  isPublic = obj.TYPE.toUpperCase() !== 'CHARTER' || obj.TYPE.toUpperCase() !== 'PRIVATE';
+  meetsMinimumEnrollment = obj.ENROLLMENT > minEnrollment;
+
+  if (obj.HAS_HIGH_SCHOOL){
+    color = '#0000FF';
+  } else if (obj.HAS_MIDDLE_SCHOOL) {
+    color = '#00FF00';
+  } else {
+    color = '##FF0000';
   }
+  // The style options
+  var pathOpts = {'radius': obj.ENROLLMENT / 30,
+                  'fillColor': color};
+  return L.circleMarker([obj.Y, obj.X], pathOpts).bindPopup(obj.FACILNAME_LABEL).addTo(map);
+})
+
 
 })();
